4f96ad00fbf24732730e5499656a89c011a597b4
Cameron Abo <cameroonie99@gmail.com>
2024-03-12 22:38:06 -0700

    3/12/24

--- widget.cpp
+++ widget.cpp
@@ -1,14 +1,124 @@
 #include "widget.h"
 #include "./ui_widget.h"
 
+
+//Constructor
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    // Set up load up state
+    ui->shipNamelineEdit->setText("Crusader, C2 Hercules Starlifter");
+    ui->cargoCapSpinBox->setValue(696);
+    ui->cargoNamelineEdit->setDisabled(true);
+    ui->priceDoubleSpinBox->setDisabled(true);
+    ui->amountSpinBox->setDisabled(true);
+    ui->buyButton->setDisabled(true);
+    ui->sellButton->setDisabled(true);
+    ui->sellAllButton->setDisabled(true);
+    ui->endButton->setDisabled(true);
+
+
+
+    // Initialize Ship Object
+    selectedShip = new Ship;
+    selectedShip->name = ui->shipNamelineEdit->text();
+    selectedShip->cargoCap = ui->cargoCapSpinBox->value();
+
+    runStopWatch = false;
+
+    hr = 00;
+    min = 00;
+    sec = 00;
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Widget::updateTimer);
+    //timer->start(1000);
 }
 
+//Deconstructor
 Widget::~Widget()
 {
     delete ui;
+    delete timer;
+    delete selectedShip;
 }
+
+void Widget::updateTimer()
+{
+    sec++;
+
+    if(sec == 60)
+    {
+        min++;
+        sec = 0;
+    }
+
+    if(min == 60)
+    {
+        hr++;
+        min = 0;
+
+    }
+
+    ui->editHrLabel->setText(hr < 10 ? "0" + QString::number(hr):QString::number(hr));
+    ui->editMinLabel->setText(min < 10 ? "0" + QString::number(min) : QString::number(min));
+    ui->editSecLabel->setText(sec < 10 ? "0" + QString::number(sec) : QString::number(sec));
+}
+
+
+// When return is pressed in shipNameLineEdit:
+//      store the current text in selectedShip.name
+void Widget::on_shipNamelineEdit_returnPressed()
+{
+    selectedShip->name = ui->shipNamelineEdit->text();
+    qDebug() << "selectedShip.name = " << selectedShip->name;
+}
+
+// If startBalDoubleSpinBox value is changed:
+//      store current value in startingBal
+void Widget::on_startBalDoubleSpinBox_valueChanged(double arg1)
+{
+    startingBal = ui->startBalDoubleSpinBox->value();
+    currentBal = startingBal;
+    ui->editCurrentBalanceLabel->setText(QString::number(currentBal) + " aUEC");
+    qDebug() << "startingBal = " << startingBal;
+    qDebug() << "CurrentBal = " << currentBal;
+}
+
+
+void Widget::on_beginButton_clicked()
+{
+    ui->endButton->setDisabled(false);
+
+    ui->startBalDoubleSpinBox->setDisabled(true);
+    ui->beginButton->setDisabled(true);
+    ui->shipNamelineEdit->setDisabled(true);
+    ui->cargoCapSpinBox->setDisabled(true);
+    ui->cargoNamelineEdit->setDisabled(false);
+    ui->priceDoubleSpinBox->setDisabled(false);
+    ui->amountSpinBox->setDisabled(false);
+    ui->buyButton->setDisabled(false);
+
+    timer->start(1000);
+}
+
+
+void Widget::on_endButton_clicked()
+{
+    ui->beginButton->setDisabled(false);
+    ui->startBalDoubleSpinBox->setDisabled(false);
+    ui->beginButton->setDisabled(false);
+    ui->shipNamelineEdit->setDisabled(false);
+    ui->cargoCapSpinBox->setDisabled(false);
+
+    ui->cargoNamelineEdit->setDisabled(true);
+    ui->priceDoubleSpinBox->setDisabled(true);
+    ui->amountSpinBox->setDisabled(true);
+    ui->buyButton->setDisabled(true);
+    ui->endButton->setDisabled(true);
+
+    timer->stop();
+}
+
--- widget.h
+++ widget.h
@@ -2,12 +2,19 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QDebug>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class Widget;
 }
 QT_END_NAMESPACE
+
+struct Ship{
+    QString name;
+    int     cargoCap;
+};
 
 class Widget : public QWidget
 {
@@ -16,8 +23,24 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+public slots:
+    void updateTimer();
 
 private:
     Ui::Widget *ui;
+    Ship* selectedShip;
+    double startingBal;
+    double currentBal;
+    bool   runStopWatch;
+    int    hr;
+    int    min;
+    int    sec;
+    QTimer* timer;
+
+private slots:
+    void on_shipNamelineEdit_returnPressed();
+    void on_startBalDoubleSpinBox_valueChanged(double arg1);
+    void on_beginButton_clicked();
+    void on_endButton_clicked();
 };
 #endif // WIDGET_H
